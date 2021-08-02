2582a22dea58acc99ae3befd0d7f16c2b1875b54
Hxxeonx <55786817+m6m112@users.noreply.github.com>
2021-08-03 03:24:48 +0900

    Update ui.py

--- Kiwoom_OPENAPI/UI/ui.py
+++ Kiwoom_OPENAPI/UI/ui.py
@@ -5,10 +5,10 @@ import sys
 
 class UIClass():                        
     def __init__(self):
-        print("UI_Class")
+        print("UI_Class start")
 
-        self.app = QApplication(sys.argv)
+        self.app = QApplication(sys.argv)       #메인 event loop 생성         
 
-        self.kiwoom = Kiwoom()
+        self.kiwoom = Kiwoom()                  #Kiwoom Class 실행 
 
-        self.app.exec_()
+        self.app.exec_()                        #event loop 실행 
